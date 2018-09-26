503bd2d380fb3634f1f46a1eb0ab2c34cffb5dfa
Artem Boray <art.war@mail.ru>
2018-09-26 12:10:32 +0300

    check

---
+++ prak_1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  prak_1
+//
+//  Created by Артём Борай on 26.09.2018.
+//  Copyright © 2018 Артём Борай. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
